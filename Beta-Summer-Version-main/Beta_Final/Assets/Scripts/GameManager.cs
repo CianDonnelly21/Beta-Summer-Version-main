f9aa3440231814ccf235959826405e58eb3ce909
CianDonnelly21 <b00160418@mytudublin.ie>
2025-08-08 21:38:04 +0100

    Edits to game and folders

---
+++ Beta-Summer-Version-main/Beta_Final/Assets/Scripts/GameManager.cs
@@ -0,0 +1,68 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+public Button PlayButton;
+public Button EasyButton;
+public Button MediumButton;
+public Button HardButton;
+public Button HelpButton;
+public Button HomeButton;
+
+
+void Start() 
+    {
+        PlayButton.onClick.AddListener(LevelOne);
+        EasyButton.onClick.AddListener(LevelOne);
+        MediumButton.onClick.AddListener(LevelTwo);
+        HardButton.onClick.AddListener(LevelThree);
+        HelpButton.onClick.AddListener(Tutorial);
+    }
+
+void Update()
+    {
+
+    }
+
+void MainMenu()
+    {
+    SceneManager.LoadScene(0);
+    }
+    
+void LevelOne()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    void LevelTwo()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    void LevelThree()
+    {
+        SceneManager.LoadScene(3);
+    }
+
+    void Win()
+    {
+        SceneManager.LoadScene(4);
+    }
+
+    void Lose()
+    {
+        SceneManager.LoadScene(5);
+    }
+
+    void Tutorial()
+    {
+        SceneManager.LoadScene(6);
+    }
+
+    void GoodLuck()
+    {
+        SceneManager.LoadScene(7);
+    }
+}
